e57ada83d687f87e579eb89a2e42aa3c24b6b8cd
robbi <robbi.orn.karlsson@gmail.com>
2018-09-18 10:46:28 +0000

    Fixed a few errors in the code, few errors stil remain

--- TileTraveller.py
+++ TileTraveller.py
@@ -2,14 +2,15 @@ tile = (1,1)
 while True:
     if tile == (1,1):
         print("You can travel: (N)orth.")
-        direction_input = input("Direction: ")
+        direction_input = input("Direction: ").lower()
         if direction_input == "n":
             tile = (1,2)
         else:
             print("Not a valid direction!")
+            return
     elif tile == (1,2):
         print("You can travel: (N)orth or (E)ast or (S)outh.")
-        direction_input = input("Direction: ")
+        direction_input = input("Direction: ").lower()
         if direction_input == "n":
             tile = (1,3)
         elif direction_input == "e":
@@ -20,7 +21,7 @@ while True:
             print("Not a valid direction!")
     elif tile == (1,3):
         print("You can travel: (E)ast or (S)outh.")
-        direction_input = input("Direction: ")
+        direction_input = input("Direction: ").lower()
         if direction_input == "e":
             tile = (2,3)
         elif direction_input == "s":
@@ -29,14 +30,14 @@ while True:
             print("Not a valid direction!")
     elif tile == (2,1):
         print("You can travel: (N)orth.")
-        direction_input = input("Direction: ")
+        direction_input = input("Direction: ").lower()
         if direction_input == "n":
             tile = (2,2)
         else:
             print("Not a valid direction!")
     elif tile == (2,2):
         print("You can travel: (W)est or (S)outh.")
-        direction_input = input("Direction: ")
+        direction_input = input("Direction: ").lower()
         if direction_input == "w":
             tile = (1,2)
         elif direction_input == "s":
@@ -44,8 +45,8 @@ while True:
         else:
             print("Not a valid direction!")
     elif tile == (2,3):
-        print("You can travel: (E)ast or (W)est:")
-        direction_input = input("Direction: ")
+        print("You can travel: (E)ast or (W)est.")
+        direction_input = input("Direction: ").lower()
         if direction_input == "e":
             tile = (3,3)
         elif direction_input == "w":
@@ -54,7 +55,7 @@ while True:
             print("Not a valid direction!")
     elif tile == (3,2):
         print("You can travel: (N)orth or (S)outh.")
-        direction_input = input("Direction: ")
+        direction_input = input("Direction: ").lower()
         if direction_input == "n":
             tile = (3,3)
         elif direction_input == "s":
@@ -63,7 +64,7 @@ while True:
             print("Not a valid direction!")
     elif tile == (3,3):
         print("You can travel: (S)outh or (W)est.")
-        direction_input = input("Direction: ")
+        direction_input = input("Direction: ").lower()
         if direction_input == "s":
             tile = (3,2)
         elif direction_input == "w":
