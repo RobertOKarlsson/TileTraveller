28c2066f18ef3f762a9d8c687aad0f6b30c41423
robbi <robbi.orn.karlsson@gmail.com>
2018-09-18 09:42:06 +0000

    skeleton code created for project

---
+++ TileTraveller.py
@@ -0,0 +1,9 @@
+direction_input = input("Direction: ")
+
+while True:
+    start_tile = (1,1)
+    n or N = (N)orth
+    s or S = (S)outh
+    w or W = (W)est 
+    e or E = (E)ast
+    print("You can travel: {}" .format(n))
