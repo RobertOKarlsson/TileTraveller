1adb1e1e78408acb60dd065330d38b324fd9cb16
robbi <robbi.orn.karlsson@gmail.com>
2018-09-21 00:05:12 +0000

    Fully coded project

---
+++ TileTravellerFunctions.py
@@ -0,0 +1,95 @@
+tile = (1,1)
+def move11():
+    direction_input = input("Direction: ")
+    if direction_input == "n":
+        tile = (1,2)
+    else:
+        print("Not a valid direction!")
+
+def traveller(tile):
+    while True:
+        direction_input = "q"
+        if tile == (1,1):
+            print("You can travel: (N)orth.")
+            while direction_input not in "n":
+                direction_input = input("Direction: ").lower()
+                if direction_input == "n":
+                    tile = (1,2)
+                else:
+                    print("Not a valid direction!")
+        elif tile == (1,2):
+            print("You can travel: (N)orth or (E)ast or (S)outh.")
+            while direction_input not in "nes":
+                direction_input = input("Direction: ").lower()
+                if direction_input == "n":
+                    tile = (1,3)
+                elif direction_input == "e":
+                    tile = (2,2)
+                elif direction_input == "s":
+                    tile = (1,1)
+                else:
+                    print("Not a valid direction!")
+        elif tile == (1,3):
+            print("You can travel: (E)ast or (S)outh.")
+            while direction_input not in "es":
+                direction_input = input("Direction: ").lower()
+                if direction_input == "e":
+                    tile = (2,3)
+                elif direction_input == "s":
+                    tile = (1,2)
+                else:
+                    print("Not a valid direction!")
+        elif tile == (2,1):
+            print("You can travel: (N)orth.")
+            while direction_input not in "n":
+                direction_input = input("Direction: ").lower()
+                if direction_input == "n":
+                    tile = (2,2)
+                else:
+                    print("Not a valid direction!")
+        elif tile == (2,2):
+            print("You can travel: (S)outh or (W)est.")
+            while direction_input not in "ws":
+                direction_input = input("Direction: ").lower()
+                if direction_input == "w":
+                    tile = (1,2)
+                elif direction_input == "s":
+                    tile = (2,1)
+                else:
+                    print("Not a valid direction!")
+        elif tile == (2,3):
+            print("You can travel: (E)ast or (W)est.")
+            while direction_input not in "ew":
+                direction_input = input("Direction: ").lower()
+                if direction_input == "e":
+                    tile = (3,3)
+                elif direction_input == "w":
+                    tile = (1,3)
+                else:
+                    print("Not a valid direction!")
+        elif tile == (3,2):
+            print("You can travel: (N)orth or (S)outh.")
+            while direction_input not in "ns":
+                direction_input = input("Direction: ").lower()
+                if direction_input == "n":
+                    tile = (3,3)
+                elif direction_input == "s":
+                    tile = (3,1)
+                else:
+                    print("Not a valid direction!")
+        elif tile == (3,3):
+            print("You can travel: (S)outh or (W)est.")
+            while direction_input not in "sw":
+                direction_input = input("Direction: ").lower()
+                if direction_input == "s":
+                    tile = (3,2)
+                elif direction_input == "w":
+                    tile = (2,3)
+                else:
+                    print("Not a valid direction!")
+        elif tile == (3,1):
+            print("Victory!")
+            break
+
+game = traveller(tile)
+
